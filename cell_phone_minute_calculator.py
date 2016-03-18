5eeb327704a2be8feb237b46c643f1577f034ee4
Ivan Boatwright <iboatwright@users.noreply.github.com>
2016-03-17 22:26:58 -0400

    Python code seems to be working.  Need to add comments.

--- cell_phone_minute_calculator.py
+++ cell_phone_minute_calculator.py
@@ -14,38 +14,58 @@ def main():
     # Declare local variables
     endProgram = 'no'
     while endProgram.lower() == 'no':
-        minutesAllowed = 0
-        minutesUsed = 0
-        totalDue = 0
-        minutesOver = 0
+        minutesAllowed = 0.0
+        minutesUsed = 0.0
+        totalDue = 0.0
+        minutesOver = 0.0
 
         # Calls functions/modules
-        minutesAllowed = getAllowed(minutesAllowed)
-        minutesUsed = getUsed(minutesUsed)
-        totalDue, minutesOver = calcTotal(minutesAllowed, minutesUsed,
-                                          totalDue, minutesOver)
+        minutesAllowed = getAllowed()
+        minutesUsed = getUsed()
+        totalDue, minutesOver = calcTotal(minutesAllowed, minutesUsed)
         display_results(minutesAllowed, minutesUsed, totalDue, minutesOver)
-        endProgram = input("Do you want to end program? yes or no")
-        while endProgram.lower() != 'yes' or endProgram.lower() != 'no':
+        endProgram = input("Do you want to end program? yes or no\n    >>>")
+        while endProgram.lower() != 'yes' and endProgram.lower() != 'no':
             print('Error: Invalid entry.')
-            endProgram = input("Do you want to end program? yes or no")
+            endProgram = input("Do you want to end program? yes or no\n    >>>")
     return None
 
 # todo: Add comments for all the functions.
-def getAllowed(minsAllowed):
-# todo: Add code for all the functions.
+def getAllowed():
+    minsAllowed = float(input('How many minutes are allowed?\n   >>> '))
+    while minsAllowed < 200 or minsAllowed > 800:
+        print('Please enter minutes between 200 and 800.')
+        minsAllowed = float(input('How many minutes are allowed?\n   >>> '))
     return minsAllowed
 
-def getUsed(minsUsed):
 
+def getUsed():
+    minsUsed = float(input('How many minutes were used?\n   >>> '))
+    while minsUsed < 0.0:
+        print('Please enter minutes of at least 0.')
+        minsUsed = float(input('How many minutes were used?\n   >>> '))
     return minsUsed
 
-def calcTotal(minsAllowed, minsUsed, totalDue, minsOver):
 
+def calcTotal(minsAllowed, minsUsed):
+    extra = 0.0
+    if minsUsed <= minsAllowed:
+        totalDue = 74.99
+        minsOver = 0
+        print('You were not over your minutes for the month')
+    else:
+        minsOver = minsUsed - minsAllowed
+        extra = minsOver * .20
+        totalDue = 74.99 + extra
+        print('You were over your minutes by {} minutes.'.format(minsOver))
     return totalDue, minsOver
 
 def display_results(minsAllowed, minsUsed, totalDue, minsOver):
-
+    print('----------------MONTHLY USE REPORT----------------------')
+    print('Minutes allowed were: {}'.format(minsAllowed))
+    print('Minutes used were: {}'.format(minsUsed))
+    print('Minutes over were: {}'.format(minsOver))
+    print('Total due is ${}'.format(totalDue))
     return None
 
 
